c04d03d0e815b090b714643d03b9301690cf3445
Sowmyacvreddy <Sowmyasomareddy@gmail.com>
2021-04-05 08:34:04 +0530

    router setup done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './default.scss';
 import Header from './component/Header/index';
 import HomePage from './pages/HomePage/index';
+import Registration from './pages/Registration';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="main">
-        <HomePage />
+        <Route exact path ="/" component={HomePage} />
+        <Route exact path ="/registration" component={Registration} />
       </div>  
     </div>
   );
